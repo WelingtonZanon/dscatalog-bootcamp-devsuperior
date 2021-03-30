3f0da0fb51a0da3bc1c7d591959832377a3b9bc1
Welington Zanon <welington_zanon@hotmail.com>
2021-03-30 12:58:42 -0300

    fontend web created

---
+++ front-web/src/app.tsx
@@ -0,0 +1,9 @@
+import './core/assets/styles/custom.scss'
+import './app.scss'
+
+const App = ()=>{
+    return <h1>Hello App</h1>
+}
+
+//nescessario exportar para por importar em outro canto
+export default App;
