db23c1b131285aaa44222483a8e526a713a60cae
Welington Zanon <welington_zanon@hotmail.com>
2021-04-01 21:32:28 -0300

    01. Projeto DSCatalog back end Parte 1 done

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -2,55 +2,68 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg'
-import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg'
-import ProductPrice from '../../../../core/components/ProductPrice';
-import { Product } from '../../../../core/types/Product';
+import ProductPrice from 'core/components/ProductPrice';
+import { Product } from 'core/types/Product';
 import { makeRequest } from '../../../../core/utils/request';
+import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader';
+import ProductInfoLoader from '../Loaders/ProductInfoLoader';
 
 import './style.scss'
 
-type ParamsType={
+type ParamsType = {
     productId: string;
 }
-const ProductDetails= ()=>{
-    const {productId} = useParams<ParamsType>();
-    const [product,setProduct] = useState<Product>();
+const ProductDetails = () => {
+    const { productId } = useParams<ParamsType>();
+    const [product, setProduct] = useState<Product>();
+    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(()=>{
-        makeRequest({url:`/products/${productId}`})
-        .then(response => setProduct(response.data));
-    },[productId]);
 
-    
+    useEffect(() => {
+        setIsLoading(true);
+        makeRequest({ url: `/products/${productId}` })
+            .then(response => setProduct(response.data))
+            .finally(() => setIsLoading(false));
+    }, [productId]);
+
+
     return (
         <div className="product-details-container">
             <div className="card-base border-radios-20 product-details">
-                 <Link to='/products'className="products-details-goback">
-                    <ArrowIcon className="icon-goback"/>
+                <Link to='/products' className="products-details-goback">
+                    <ArrowIcon className="icon-goback" />
                     <h1 className="text-goback">voltar</h1>
-                 </Link>
-                 <div className="row">
-                     <div className="col-6 pr-5">
-                        <div className="product-details-card border-radios-20 text-center">
-                            <img src={product?.imgUrl} alt={product?.imgUrl}  className="product-details-image"/>
-                        </div>
-                        <h1 className="product-details-name">
-                            {product?.name}
+                </Link>
+                <div className="row">
+                    <div className="col-6 pr-5">
+                        {isLoading ? <ProductInfoLoader /> : (
+                            <>
+                                <div className="product-details-card border-radios-20 text-center">
+                                    <img src={product?.imgUrl} alt={product?.imgUrl} className="product-details-image" />
+                                </div>
+                                <h1 className="product-details-name">
+                                    {product?.name}
+                                </h1>
+                                {product?.price && <ProductPrice price={product?.price} />}
+                            </>
+                        )}
+                    </div>
+                    <div className="col-6 product-details-card border-radios-20">
+                        {isLoading ? <ProductDescriptionLoader /> : (
+                            <>
+                                <h1 className="procut-description-title">
+                                    Decrição do Produto
                         </h1>
-                            {product?.price&&<ProductPrice price={product?.price}/>}
-                     </div>
-                     <div className="col-6 product-details-card border-radios-20">
-                       <h1 className="procut-description-title">
-                            Decrição do Produto
-                        </h1>
-                        <p className="product-descripiton-text">
-                            {product?.description}
-                         </p>
-                     </div>
-                 </div>
+                                <p className="product-descripiton-text">
+                                    {product?.description}
+                                </p>
+                            </>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
-    );  
+    );
 
 };
 
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -4,33 +4,40 @@ import ProductCard from './components/ProductCard';
 import './styles.scss';
 import { makeRequest } from '../../core/utils/request';
 import { ProductsReponse } from '../../core/types/Product';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 const Catalog = () => {
 
     const [productsResponse, setProductsReponse] = useState<ProductsReponse>();
+    const [isLoading, setIsLoading] = useState(false);
     //console.log(productsResponse);
     /* useEffect é Hook (função) para acessa ciclo de
      vida do pomponente. Recebe uma função e um array.
     **/
-    useEffect(()=>{
-    /*fetch (api) JS para entregar response entity (promises)
-    Limitações - muito verboso/No progress UPload e No query strings nativo
-    
-        fetch('http://localhost:3000/products')
-        .then(response => response.json())
-        .then(response => console.log(response));
-    forma inline de usar o axios    
-        axios('http://localhost:3000/products')
+    useEffect(() => {
+        /*fetch (api) JS para entregar response entity (promises)
+        Limitações - muito verboso/No progress UPload e No query strings nativo
+        
+            fetch('http://localhost:3000/products')
+            .then(response => response.json())
             .then(response => console.log(response));
-     makeRequest forma mais profissional de usar o axios
-     criando um core para chamar em execução       
-         **/ 
+        forma inline de usar o axios    
+            axios('http://localhost:3000/products')
+                .then(response => console.log(response));
+         makeRequest forma mais profissional de usar o axios
+         criando um core para chamar em execução       
+             **/
         const params = {
-            page:0,
-            linesPerPage:12
+            page: 0,
+            linesPerPage: 16
         }
-            makeRequest({url:'/products',params})
-            .then(response => setProductsReponse(response.data));
-    },[]);
+        //inicia o loader antes de carregar os itens
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsReponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, []);
 
     return (
 
@@ -39,14 +46,14 @@ const Catalog = () => {
                 Catálogo de Produtos
                 </h1>
             <div className="catalog-products">
-                
-                {productsResponse?.content.map(product =>(
-                    //passar key obrigatoria para a listagem
-                    <Link to={`products/${product.id}`} key={product.id}>
-                    <ProductCard product={product} />
-                    </Link>
-                ))}
-               
+                {isLoading ? <ProductCardLoader /> : (
+                    productsResponse?.content.map(product => (
+                        //passar key obrigatoria para a listagem
+                        <Link to={`products/${product.id}`} key={product.id}>
+                            <ProductCard product={product} />
+                        </Link>
+                    ))
+                )}
             </div>
         </div>
     );
