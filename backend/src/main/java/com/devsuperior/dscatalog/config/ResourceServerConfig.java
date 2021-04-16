0e61b0731dd413654ea025f32c88a4333fbb50b9
Welington Zanon <welington_zanon@hotmail.com>
2021-04-15 23:06:27 -0300

    ready to prod

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,15 +1,16 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
 
 //implementando o resource server authorization
 
@@ -18,10 +19,15 @@ import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
+	
+	@Autowired
+	Environment env;
+	
+	
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token","/h2/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 	
@@ -36,6 +42,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+
+		//H2 liberation
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+		http.headers().frameOptions().disable(); }
+		
+		
+		
 		// configuração das rotas
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
